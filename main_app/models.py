eb9b0edefc112c4ec83ad35dac13c59f0fa31ff8
noura <noura.tu19@gmail.com>
2025-05-05 23:46:08 +0300

    Create model ServiceListing

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# Category
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
@@ -10,3 +11,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    # ---------------------------------------------------
+    # ServiceListing
+class ServiceListing(models.Model):
+    provider = models.ForeignKey(User, related_name='service_listings', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='service_listings', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price_description = models.CharField(max_length=100)
+    location_description = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
